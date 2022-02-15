4bae927f0152173e08032b9faeba223b36ee91dd
Andrew Wilson <Andrew.Wilson@oxfordcc.co.uk>
2022-02-15 14:59:32 +0000

    mc-9774 - add method for encoding usernames as folder names and add update service tests.

--- src/app/core/user-requests.service.spec.ts
+++ src/app/core/user-requests.service.spec.ts
@@ -1,16 +1,31 @@
-import { TestBed } from '@angular/core/testing';
+import { MdmFolderResource } from '@maurodatamapper/mdm-resources';
+import { createMdmEndpointsStub } from '../testing/stubs/mdm-endpoints.stub';
+import { setupTestModuleForService } from '../testing/testing.helpers';
 
 import { UserRequestsService } from './user-requests.service';
 
 describe('UserRequestsService', () => {
   let service: UserRequestsService;
+  const endpointsStub = createMdmEndpointsStub();
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(UserRequestsService);
+    service = setupTestModuleForService(UserRequestsService, {
+      providers: [
+        {
+          provide: MdmFolderResource,
+          useValue: endpointsStub,
+        },
+      ],
+    });
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it("should replace '@' with '[at]'", () => {
+    let username = 'hello@gmail.com';
+    let encodedUsername = service.encodeUserName(username);
+    expect(encodedUsername).toBe('hello[at]gmail.com');
   });
 });
