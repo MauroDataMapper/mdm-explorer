a6828a1cf84ae614053719fab25cfe880b12fb5f
Andrew Wilson <Andrew.Wilson@oxfordcc.co.uk>
2022-02-17 12:20:24 +0000

    mc-9774 - update formatting after settings change in app.component.ts, tidy up user requests service.

--- src/app/core/user-requests.service.ts
+++ src/app/core/user-requests.service.ts
@@ -1,24 +1,24 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
+import { environment } from 'src/environments/environment';
 import { MdmEndpointsService } from '../mdm-rest-client/mdm-endpoints.service';
 import { UserDetails } from '../security/user-details.service';
-import { BroadcastService } from './broadcast.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UserRequestsService {
-  constructor(
-    private broadcast: BroadcastService,
-    private endpoints: MdmEndpointsService
-  ) {}
+  constructor(private endpoints: MdmEndpointsService) {}
 
   /**
    * Ensure the data access requests folder exists for the signed in user.
    * @param user - the details of the signed in user
    */
   ensureUserRequestsFolderExists(user: UserDetails): void {
-    let folderId = this.encodeUsername(user.userName);
+    let folderId = `${environment.rootRequestFolder}/${this.encodeUsername(
+      user.userName
+    )}`;
+
     this.folderExists(folderId).subscribe((exists: Boolean) => {
       if (exists) return;
       this.makeFolder(folderId);
@@ -44,12 +44,12 @@ export class UserRequestsService {
   }
 
   /**
-   * Checks whether a folder exists.
+   * Check whether a folder exists.
    * @returns An observable that returns `true` if the folder exists, 'false' if not.
    */
   private folderExists(folderId: string): Observable<boolean> {
     // GET: folder with given folderId
-    //  this.endpoints.folder.get(folderId).subscribe();
+    // this.endpoints.folder.get(folderId).subscribe();
     return of(true);
   }
 }
