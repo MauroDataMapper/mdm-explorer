4525adb90e12ca55bcce588cae31ec47ca258e15
Andrew Wilson <Andrew.Wilson@oxfordcc.co.uk>
2022-03-03 11:48:00 +0000

    mc-9774 - initial commit of user-requests tests, update endpoints stub to include folder service stubs.

--- src/app/core/user-requests.service.spec.ts
+++ src/app/core/user-requests.service.spec.ts
@@ -1,5 +1,8 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { MdmFolderResource } from '@maurodatamapper/mdm-resources';
+import { cold } from 'jest-marbles';
 import { MdmEndpointsService } from '../mdm-rest-client/mdm-endpoints.service';
+import { UserDetails } from '../security/user-details.service';
 import { createMdmEndpointsStub } from '../testing/stubs/mdm-endpoints.stub';
 import { setupTestModuleForService } from '../testing/testing.helpers';
 
@@ -24,15 +27,33 @@ describe('UserRequestsService', () => {
     expect(service).toBeTruthy();
   });
 
-  it("should replace '@' with '[at]'", () => {
-    let username = 'hello@gmail.com';
+  it('should be named with [at]', () => {
+    const username = 'test@gmail.com';
 
-    // By using array access, we can get at the private method
-    // we'd like to test. This is in fact an intentional 'escape hatch'
-    // built into TS to give access to private members without losing
-    // type safety. The predominant use case being for unit testing.
-    let encodedUsername = service['encodeUsername'](username);
+    const expected$ = cold('--a|', {
+      a: {
+        label: 'asdf[at]gmail.com',
+      },
+    });
 
-    expect(encodedUsername).toBe('hello[at]gmail.com');
+    const actual$ = service.getUserRequestsFolder(username);
+    expect(actual$).toBeObservable(expected$);
+  });
+
+  // Can we only simulate up to the endpoint call? I guess we can mock that
+  // something comes back but what if that's what we're testing.
+  it('should create root folder if not exists', () => {
+    // Arrange
+    // Create folder label
+    endpointsStub.folder.save.mockImplementationOnce(() => {});
+
+    //endpointsStub.folder.save.mockImplementationOnce(() => {});
+    // Act
+    //
+
+    //const actual$ = service.getUserRequestsFolder(user);
+
+    // Assert
+    //expect(endpointsStub.folder.save).toBeCalledWith();
   });
 });
