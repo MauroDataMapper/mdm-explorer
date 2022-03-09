e2cc268a0b3cada0ed597373d660ef141d99a131
Jon Bowen <jon.bowen@oxfordcc.co.uk>
2022-03-09 12:03:09 +0000

    mc-9811: corrupt license header replaced.

--- src/app/shared/header/header.component.spec.ts
+++ src/app/shared/header/header.component.spec.ts
@@ -6,7 +6,7 @@ Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
-		http://www.apache.org/licenses/LICENSE-2.0
+    http://www.apache.org/licenses/LICENSE-2.0
 
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
@@ -17,55 +17,58 @@ limitations under the License.
 SPDX-License-Identifier: Apache-2.0
 */
 import { TestBed } from '@angular/core/testing';
-import { ComponentHarness, setupTestModuleForComponent } from '../../testing/testing.helpers';
+import {
+  ComponentHarness,
+  setupTestModuleForComponent,
+} from '../../testing/testing.helpers';
 import { HeaderComponent } from './header.component';
 import { arrowDirection, ArrowPipe } from '../pipes/arrow.pipe';
 
 describe('HeaderComponent', () => {
-	let harness: ComponentHarness<HeaderComponent>;
+  let harness: ComponentHarness<HeaderComponent>;
 
   beforeEach(async () => {
     TestBed.configureTestingModule({
-      providers: [ ArrowPipe, ],
-      declarations: [ ArrowPipe, ],
+      providers: [ArrowPipe],
+      declarations: [ArrowPipe],
     });
     harness = await setupTestModuleForComponent(HeaderComponent);
     TestBed.inject(ArrowPipe);
-	});
+  });
 
-	it('should create', () => {
-		expect(harness.isComponentCreated).toBeTruthy();
-		expect(harness.component.links).toEqual([]);
-		expect(harness.component.includeSignIn).toBeFalsy();
-		expect(harness.component.isSignedIn).toBeFalsy();
-	});
+  it('should create', () => {
+    expect(harness.isComponentCreated).toBeTruthy();
+    expect(harness.component.links).toEqual([]);
+    expect(harness.component.includeSignIn).toBeFalsy();
+    expect(harness.component.isSignedIn).toBeFalsy();
+  });
 
-	it('should include sign-in when link provided', () => {
-		harness.component.signInLink = {
-			routeName: 'test',
-			label: 'Sign in'
-		};
+  it('should include sign-in when link provided', () => {
+    harness.component.signInLink = {
+      routeName: 'test',
+      label: 'Sign in',
+    };
 
-		expect(harness.component.includeSignIn).toBeTruthy();
-	});
+    expect(harness.component.includeSignIn).toBeTruthy();
+  });
 
-	it('should be signed in when user provided', () => {
-		harness.component.signedInUser = {
-			id: '1',
-			userName: 'test',
-			firstName: 'Test',
-			lastName: 'Person',
-			email: 'test@test.com'
-		};
+  it('should be signed in when user provided', () => {
+    harness.component.signedInUser = {
+      id: '1',
+      userName: 'test',
+      firstName: 'Test',
+      lastName: 'Person',
+      email: 'test@test.com',
+    };
 
-		expect(harness.component.isSignedIn).toBeTruthy();
+    expect(harness.component.isSignedIn).toBeTruthy();
   });
 
   it('should display Sign In link when not signed in', () => {
     const dom = harness.fixture.nativeElement;
     harness.component.signInLink = {
-			routeName: 'test',
-			label: 'Sign in'
+      routeName: 'test',
+      label: 'Sign in',
     };
     harness.detectChanges();
     const signInElement = dom.querySelector('#signIn');
@@ -81,16 +84,16 @@ describe('HeaderComponent', () => {
   it('should display Sign Out link, bookmarks and requests when signed in', () => {
     const dom = harness.fixture.nativeElement;
     harness.component.signInLink = {
-			routeName: 'test',
-			label: 'Sign in'
+      routeName: 'test',
+      label: 'Sign in',
     };
-		harness.component.signedInUser = {
-			id: '1',
-			userName: 'test',
-			firstName: 'Test',
-			lastName: 'Person',
-			email: 'test@test.com'
-		};
+    harness.component.signedInUser = {
+      id: '1',
+      userName: 'test',
+      firstName: 'Test',
+      lastName: 'Person',
+      email: 'test@test.com',
+    };
     harness.component.rightLinks = [
       {
         label: 'Bookmarks',
@@ -102,7 +105,7 @@ describe('HeaderComponent', () => {
       label: 'My requests',
       routeName: 'app.container.my-requests',
       arrow: arrowDirection.angle_down,
-      };
+    };
     harness.detectChanges();
     // check that the elements have the correct existence
     const signInElement = dom.querySelector('#signIn');
