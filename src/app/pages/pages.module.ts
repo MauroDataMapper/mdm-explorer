7ae8778a9602b398027c6d183af0b3fb898a498c
Andrew Wilson <Andrew.Wilson@oxfordcc.co.uk>
2022-03-15 12:25:14 +0000

    mc-9823 - fixing imports after renaming.

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -33,7 +33,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import { CatalogueSearchComponent } from './catalogue-search/catalogue-search.component';
+import { SearchComponent } from './search/search.component';
 
 @NgModule({
   declarations: [
@@ -44,7 +44,7 @@ import { CatalogueSearchComponent } from './catalogue-search/catalogue-search.co
     BrowseComponent,
     SearchResultsComponent,
     MyAccountComponent,
-    CatalogueSearchComponent,
+    SearchComponent,
   ],
   imports: [
     CommonModule,
