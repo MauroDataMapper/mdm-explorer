f38843c6c1adbb82961b84c58ecb91fc162a5c20
Aaron Forshaw <aaron@janda.org.uk>
2022-04-13 15:55:54 +0100

    mc-9776 add the component to search results

--- src/app/shared/data-element-in-request/data-element-in-request.component.ts
+++ src/app/shared/data-element-in-request/data-element-in-request.component.ts
@@ -21,7 +21,7 @@ import { DataModel, DataModelSubsetPayload } from '@maurodatamapper/mdm-resource
 import { forkJoin, switchMap } from 'rxjs';
 import { StateRouterService } from 'src/app/core/state-router.service';
 import { DataRequestsService } from 'src/app/data-explorer/data-requests.service';
-import { DataElementDetail, Uuid } from '@maurodatamapper/mdm-resources';
+import { Uuid } from '@maurodatamapper/mdm-resources';
 import { SecurityService } from 'src/app/security/security.service';
 import { MatCheckboxChange } from '@angular/material/checkbox';
 import { MdmEndpointsService } from 'src/app/mauro/mdm-endpoints.service';
@@ -33,7 +33,8 @@ import { DataModelService } from 'src/app/mauro/data-model.service';
   styleUrls: ['./data-element-in-request.component.scss'],
 })
 export class DataElementInRequestComponent implements OnInit {
-  @Input() dataElement?: DataElementDetail;
+  @Input() dataElementId: Uuid = '';
+  @Input() dataModelId: Uuid = '';
 
   dataAccessRequests: DataModel[] = [];
 
@@ -74,8 +75,8 @@ export class DataElementInRequestComponent implements OnInit {
           const checks: any[] = [];
 
           this.dataAccessRequests.forEach((item: DataModel) => {
-            if (this.dataElement && this.dataElement.model && item.id) {
-              checks.push(this.checkIntersection(this.dataElement.model, item.id));
+            if (item.id) {
+              checks.push(this.checkIntersection(this.dataModelId, item.id));
               checkedTargetDataModelIds.push(item.id);
             }
           });
@@ -88,7 +89,7 @@ export class DataElementInRequestComponent implements OnInit {
           const mySourceDataModelId = checkedTargetDataModelIds[i];
           const intersections = intersectionsForEachTargetDataModel[i];
 
-          if (intersections.intersects.indexOf(this.dataElement?.id) > -1) {
+          if (intersections.intersects.indexOf(this.dataElementId) > -1) {
             this.inRequests.push(mySourceDataModelId);
           }
           this.ready = true;
@@ -107,24 +108,22 @@ export class DataElementInRequestComponent implements OnInit {
    * @param event
    */
   changed(event: MatCheckboxChange) {
-    if (this.dataElement && this.dataElement.id && this.dataElement.model) {
-      const targetDataModelId = event.source.value;
-      const datamodelSubsetPayload: DataModelSubsetPayload = {
-        additions: [],
-        deletions: [],
-      };
-      if (event.checked) {
-        // Do a subset add for this data element in the request
-        datamodelSubsetPayload.additions = [this.dataElement.id];
-      } else {
-        // Do a subset remove for this data element in the request
-        datamodelSubsetPayload.deletions = [this.dataElement.id];
-      }
-
-      this.endpoints.dataModel
-        .copySubset(this.dataElement.model, targetDataModelId, datamodelSubsetPayload) // eslint-disable-line @typescript-eslint/no-unsafe-argument
-        .subscribe(() => {});
+    const targetDataModelId = event.source.value;
+    const datamodelSubsetPayload: DataModelSubsetPayload = {
+      additions: [],
+      deletions: [],
+    };
+    if (event.checked) {
+      // Do a subset add for this data element in the request
+      datamodelSubsetPayload.additions = [this.dataElementId];
+    } else {
+      // Do a subset remove for this data element in the request
+      datamodelSubsetPayload.deletions = [this.dataElementId];
     }
+
+    this.endpoints.dataModel
+      .copySubset(this.dataModelId, targetDataModelId, datamodelSubsetPayload)
+      .subscribe(() => {}); // eslint-disable-line @typescript-eslint/no-unsafe-argument
   }
 
   /**
