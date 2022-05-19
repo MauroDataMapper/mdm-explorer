e5546e738cef8e629c10be3238d356a37aa44366
Aaron Forshaw <aaron@janda.org.uk>
2022-05-19 17:08:33 +0100

    mc-9884 change api path

--- src/app/mauro/plugins/plugin-research.resource.ts
+++ src/app/mauro/plugins/plugin-research.resource.ts
@@ -81,7 +81,7 @@ export class MdmPluginResearchResource extends MdmResource {
    * `200 OK` - will return an folder detail response. The HTTP status explains that it was successful.
    */
   userFolder(options?: RequestSettings) {
-    const url = `${this.apiEndpoint}/userFolder`;
+    const url = `${this.apiEndpoint}/explorer/userFolder`;
     return this.simplePost(url, null, options);
   }
 }
