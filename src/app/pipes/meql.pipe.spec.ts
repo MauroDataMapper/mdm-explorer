18b314cdf22ac64ecc9f92fbe2f1d7437ab309b8
Nigel Palmer <nigel.palmer@oxfordcc.co.uk>
2022-12-13 13:18:42 +0000

    gh-173: Refactored to be data driven tests.

--- src/app/pipes/meql.pipe.spec.ts
+++ src/app/pipes/meql.pipe.spec.ts
@@ -23,75 +23,20 @@ import moment from 'moment';
 const newline = '\r\n';
 const tab = '\t';
 
-class meqlTestHelper {
-  public static singleConditionTest(
-    pipe: MeqlPipe,
-    field: string,
-    operator: string,
-    value: any
-  ) {
-    /*
-  Expected example:
-
-  (
-    "Field" = "Value"
-  )
-   */
-    const query = {
-      condition: 'and',
-      rules: [{ field, operator, value }],
-    };
-    const line1 = `(${newline}`;
-    const line2 = `${tab}"${query.rules[0].field}" ${query.rules[0].operator} "${query.rules[0].value}"${newline}`;
-    const line3 = ')';
-    return [pipe.transform(query), `${line1}${line2}${line3}`];
-  }
-
-  public static doubleConditionTest(pipe: MeqlPipe, query: any) {
-    /*
-  Expected example:
-
-  (
-    "Field" = "Value"
-    and "Field2" = "Value2"
-  )
-   */
-    const line1 = `(${newline}`;
-    const line2 = `${tab}"${query.rules[0].field}" ${query.rules[0].operator} "${query.rules[0].value}"${newline}`;
-    const line3 = `${tab}${query.condition} "${query.rules[1].field}" ${query.rules[1].operator} "${query.rules[1].value}"${newline}`;
-    const line4 = ')';
-    return [pipe.transform(query), `${line1}${line2}${line3}${line4}`];
-  }
-
-  public static doubleRulesetTest(pipe: MeqlPipe, query: any) {
-    /*
-  Expected example:
-
-  (
-    "Field" = "Value"
-    and "Field2" = "Value2"
-    and (
-        "Field3" = "Value3"
-        or "Field4" = "Value4"
-    )
-  )
-   */
-    const line1 = `(${newline}`;
-    const line2 = `${tab}"${query.rules[0].field}" ${query.rules[0].operator} "${query.rules[0].value}"${newline}`;
-    const line3 = `${tab}${query.condition} "${query.rules[1].field}" ${query.rules[1].operator} "${query.rules[1].value}"${newline}`;
-    const line4 = `${tab}${query.condition} (${newline}`;
-    const line5 = `${tab}${tab}"${query.rules[2].rules[0].field}" ${query.rules[2].rules[0].operator} "${query.rules[2].rules[0].value}"${newline}`;
-    const line6 = `${tab}${tab}${query.rules[2].condition} "${query.rules[2].rules[1].field}" ${query.rules[2].rules[1].operator} "${query.rules[2].rules[1].value}"${newline}`;
-    const line7 = `${tab})${newline}`;
-    const line8 = ')';
-    return [
-      pipe.transform(query),
-      `${line1}${line2}${line3}${line4}${line5}${line6}${line7}${line8}`,
-    ];
-  }
+interface Expression {
+  field: string;
+  operator: '=' | '!='; /* We don't need to test every type of operator. */
+  value: string;
 }
 
-describe('JqlPipe', () => {
+interface Condition {
+  condition: 'and' | 'or';
+  rules: Rule[];
+}
+
+type Rule = Expression | Condition;
+
+describe('MeqlPipe', () => {
   let pipe: MeqlPipe;
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -100,257 +45,298 @@ describe('JqlPipe', () => {
     pipe = TestBed.inject(MeqlPipe);
   });
 
-  it('create an instance', () => {
-    expect(pipe).toBeTruthy();
+  describe('Basic Tests', () => {
+    it('create an instance', () => {
+      expect(pipe).toBeTruthy();
+    });
   });
 
-  it('transforms field equal to string', () => {
-    const testResult = meqlTestHelper.singleConditionTest(
-      pipe,
-      'Simple String Field',
-      '=',
-      'Some Text'
-    );
-    expect(testResult[0]).toBe(testResult[1]);
+  describe('Single Condition Tests', () => {
+    it.each([
+      ['transforms field equal to string', 'Simple String Field', '=', 'Some Text'],
+      ['transforms field not equal to string', 'Simple String Field', '!=', 'Some Text'],
+      ['transforms field equal to number', 'Simple String Field', '=', 0],
+      ['transforms field not equal to number', 'Simple String Field', '!=', 99],
+    ])
+    (
+     '%s',
+     (testname, field, operator, value) => {
+        /*
+          Expected example:
+
+          (
+            "Field" = "Value"
+          )
+        */
+        const query = {
+          condition: 'and',
+          rules: [{ field, operator, value }],
+        };
+        const line1 = `(${newline}`;
+        const line2 = `${tab}"${query.rules[0].field}" ${query.rules[0].operator} "${query.rules[0].value}"${newline}`;
+        const line3 = ')';
+
+        const actual =  pipe.transform(query);
+        const expected = `${line1}${line2}${line3}`;
+        expect(actual).toBe(expected);
+     });
   });
 
-  it('transforms field not equal to string', () => {
-    const testResult = meqlTestHelper.singleConditionTest(
-      pipe,
-      'Simple String Field',
-      '!=',
-      'Some Text'
-    );
-    expect(testResult[0]).toBe(testResult[1]);
-  });
-
-  it('transforms field equal to number', () => {
-    const testResult = meqlTestHelper.singleConditionTest(
-      pipe,
-      'Simple String Field',
-      '=',
-      0
-    );
-    expect(testResult[0]).toBe(testResult[1]);
-  });
-
-  it('transforms field not equal to number', () => {
-    const testResult = meqlTestHelper.singleConditionTest(
-      pipe,
-      'Simple String Field',
-      '!=',
-      99
-    );
-    expect(testResult[0]).toBe(testResult[1]);
-  });
-
-  it('transforms and uses "and" condition', () => {
-    const query = {
-      condition: 'and',
-      rules: [
-        {
-          field: 'Simple String Field',
-          operator: '=',
-          value: 'String 1',
-        },
-        {
-          field: 'String field 2',
-          operator: '!=',
-          value: 'String 2',
-        },
-      ],
-    };
-    const testResult = meqlTestHelper.doubleConditionTest(pipe, query);
-    expect(testResult[0]).toBe(testResult[1]);
-  });
-
-  it('transforms and uses "or" condition', () => {
-    const query = {
-      condition: 'or',
-      rules: [
-        {
-          field: 'Simple String Field',
-          operator: '=',
-          value: 'String 1',
-        },
-        {
-          field: 'String field 2',
-          operator: '!=',
-          value: 'String 2',
-        },
-      ],
-    };
-    const testResult = meqlTestHelper.doubleConditionTest(pipe, query);
-    expect(testResult[0]).toBe(testResult[1]);
-  });
-
-  it('transforms and uses "and" and "or" conditions', () => {
-    const query = {
-      condition: 'and',
-      rules: [
-        {
-          field: 'String field 1',
-          operator: '=',
-          value: 'String 1',
-        },
-        {
-          field: 'String field 2',
-          operator: '!=',
-          value: 'String 2',
-        },
-        {
-          condition: 'or',
-          rules: [
-            {
-              field: 'Simple field 3',
-              operator: '=',
-              value: 'String 3',
-            },
-            {
-              field: 'String field 4',
-              operator: '!=',
-              value: 'String 4',
-            },
-          ],
-        },
-      ],
-    };
-    const testResult = meqlTestHelper.doubleRulesetTest(pipe, query);
-    expect(testResult[0]).toBe(testResult[1]);
-  });
-
-  it('transforms and uses "or" and "and" conditions', () => {
-    const query = {
-      condition: 'or',
-      rules: [
-        {
-          field: 'String field 1',
-          operator: '=',
-          value: 'String 1',
-        },
-        {
-          field: 'String field 2',
-          operator: '!=',
-          value: 'String 2',
-        },
+  describe('Double Condition Tests', () => {
+    it.each([
+      [
+        'transforms and uses "and" condition',
         {
           condition: 'and',
           rules: [
             {
-              field: 'Simple field 3',
+              field: 'Simple String Field',
               operator: '=',
-              value: 'String 3',
+              value: 'String 1',
             },
             {
-              field: 'String field 4',
+              field: 'String field 2',
               operator: '!=',
-              value: 'String 4',
+              value: 'String 2',
             },
           ],
-        },
+        }
       ],
-    };
-    const testResult = meqlTestHelper.doubleRulesetTest(pipe, query);
-    expect(testResult[0]).toBe(testResult[1]);
-  });
-
-  it('transforms and uses "or" and "or" conditions', () => {
-    const query = {
-      condition: 'and',
-      rules: [
-        {
-          field: 'String field 1',
-          operator: '=',
-          value: 'String 1',
-        },
-        {
-          field: 'String field 2',
-          operator: '!=',
-          value: 'String 2',
-        },
+      [
+        'transforms and uses "or" condition',
         {
           condition: 'or',
           rules: [
             {
-              field: 'Simple field 3',
+              field: 'Simple String Field',
               operator: '=',
-              value: 'String 3',
+              value: 'String 1',
             },
             {
-              field: 'String field 4',
+              field: 'String field 2',
               operator: '!=',
-              value: 'String 4',
+              value: 'String 2',
             },
           ],
-        },
+        }
       ],
-    };
-    const testResult = meqlTestHelper.doubleRulesetTest(pipe, query);
-    expect(testResult[0]).toBe(testResult[1]);
+    ])
+    (
+     '%s',
+     (testname, query) => {
+        /*
+        Expected example:
+
+        (
+          "Field" = "Value"
+          and "Field2" = "Value2"
+        )
+        */
+        const line1 = `(${newline}`;
+        const line2 = `${tab}"${query.rules[0].field}" ${query.rules[0].operator} "${query.rules[0].value}"${newline}`;
+        const line3 = `${tab}${query.condition} "${query.rules[1].field}" ${query.rules[1].operator} "${query.rules[1].value}"${newline}`;
+        const line4 = ')';
+
+        const actual =  pipe.transform(query);
+        const expected = `${line1}${line2}${line3}${line4}`;
+        expect(actual).toBe(expected);
+     });
   });
 
-  it('transforms and uses "and" and "and" conditions', () => {
-    const query = {
-      condition: 'and',
-      rules: [
+  describe('Double Ruleset Tests', () => {
+    it.each<[string, Condition]>([
+      [
+        'transforms and uses "and" and "or" conditions',
         {
-          field: 'String field 1',
-          operator: '=',
-          value: 'String 1',
-        },
-        {
-          field: 'String field 2',
-          operator: '!=',
-          value: 'String 2',
-        },
+          condition: 'and',
+          rules: [
+            {
+              field: 'String field 1',
+              operator: '=',
+              value: 'String 1',
+            },
+            {
+              field: 'String field 2',
+              operator: '!=',
+              value: 'String 2',
+            },
+            {
+              condition: 'or',
+              rules: [
+                {
+                  field: 'Simple field 3',
+                  operator: '=',
+                  value: 'String 3',
+                },
+                {
+                  field: 'String field 4',
+                  operator: '!=',
+                  value: 'String 4',
+                },
+              ],
+            },
+          ],
+        }
+      ],
+      [
+        'transforms and uses "or" and "and" conditions',
         {
           condition: 'or',
           rules: [
             {
-              field: 'Simple field 3',
+              field: 'String field 1',
               operator: '=',
-              value: 'String 3',
+              value: 'String 1',
             },
             {
-              field: 'String field 4',
+              field: 'String field 2',
               operator: '!=',
-              value: 'String 4',
+              value: 'String 2',
+            },
+            {
+              condition: 'and',
+              rules: [
+                {
+                  field: 'Simple field 3',
+                  operator: '=',
+                  value: 'String 3',
+                },
+                {
+                  field: 'String field 4',
+                  operator: '!=',
+                  value: 'String 4',
+                },
+              ],
             },
           ],
-        },
+        }
       ],
-    };
-    const testResult = meqlTestHelper.doubleRulesetTest(pipe, query);
-    expect(testResult[0]).toBe(testResult[1]);
-  });
-
-  it('formats dates correctly', () => {
-    const query = {
-      condition: 'and',
-      rules: [
+      [
+        'transforms and uses "or" and "or" conditions',
         {
-          field: 'A Date Field',
-          operator: '=',
-          value: moment('12/31/2022'),
-        },
+          condition: 'or',
+          rules: [
+            {
+              field: 'String field 1',
+              operator: '=',
+              value: 'String 1',
+            },
+            {
+              field: 'String field 2',
+              operator: '!=',
+              value: 'String 2',
+            },
+            {
+              condition: 'or',
+              rules: [
+                {
+                  field: 'Simple field 3',
+                  operator: '=',
+                  value: 'String 3',
+                },
+                {
+                  field: 'String field 4',
+                  operator: '!=',
+                  value: 'String 4',
+                },
+              ],
+            },
+          ],
+        }
       ],
-    };
+      [
+        'transforms and uses "and" and "and" conditions',
+        {
+          condition: 'and',
+          rules: [
+            {
+              field: 'String field 1',
+              operator: '=',
+              value: 'String 1',
+            },
+            {
+              field: 'String field 2',
+              operator: '!=',
+              value: 'String 2',
+            },
+            {
+              condition: 'and',
+              rules: [
+                {
+                  field: 'Simple field 3',
+                  operator: '=',
+                  value: 'String 3',
+                },
+                {
+                  field: 'String field 4',
+                  operator: '!=',
+                  value: 'String 4',
+                },
+              ],
+            },
+          ],
+        }
+      ]
+    ])
+    (
+     '%s',
+     (testname, query) => {
+        /*
+        Expected example:
 
-    const line1 = `(${newline}`;
-    const line2 = `${tab}"A Date Field" = "31/12/2022"${newline}`;
-    const line3 = ')';
-    const expected = `${line1}${line2}${line3}`;
+        (
+          "Field" = "Value"
+          and "Field2" = "Value2"
+          and (
+              "Field3" = "Value3"
+              or "Field4" = "Value4"
+          )
+        )
+        */
+        const line1 = `(${newline}`;
+        const line2 = `${tab}"${(query.rules[0] as Expression).field}" ${(query.rules[0] as Expression).operator} "${(query.rules[0] as Expression).value}"${newline}`;
+        const line3 = `${tab}${query.condition} "${(query.rules[1] as Expression).field}" ${(query.rules[1] as Expression).operator} "${(query.rules[1] as Expression).value}"${newline}`;
+        const line4 = `${tab}${query.condition} (${newline}`;
+        const line5 = `${tab}${tab}"${((query.rules[2] as Condition).rules[0] as Expression).field}" ${((query.rules[2] as Condition).rules[0] as Expression).operator} "${((query.rules[2] as Condition).rules[0] as Expression).value}"${newline}`;
+        const line6 = `${tab}${tab}${(query.rules[2] as Condition).condition} "${((query.rules[2] as Condition).rules[1] as Expression).field}" ${((query.rules[2] as Condition).rules[1] as Expression).operator} "${((query.rules[2] as Condition).rules[1] as Expression).value}"${newline}`;
+        const line7 = `${tab})${newline}`;
+        const line8 = ')';
 
-    const actual = pipe.transform(query);
-    expect(actual).toBe(expected);
+        const expected = `${line1}${line2}${line3}${line4}${line5}${line6}${line7}${line8}`;
+        const actual = pipe.transform(query);
+        expect(actual).toBe(expected);
+      });
   });
 
-  it('returns empty string for invalid data', () => {
-    const query = {
-      unexpectedField: 'should not be here',
-      unexpectedArray: [{ badField: 'not valid' }],
-    };
-    const jql = pipe.transform(query);
-    expect(jql).toBe('');
+  describe('Date Tests', () => {
+    it('formats dates correctly', () => {
+      const query = {
+        condition: 'and',
+        rules: [
+          {
+            field: 'A Date Field',
+            operator: '=',
+            value: moment('12/31/2022', 'MM/DD/YYYY'),
+          },
+        ],
+      };
+
+      const line1 = `(${newline}`;
+      const line2 = `${tab}"A Date Field" = "31/12/2022"${newline}`;
+      const line3 = ')';
+
+      const expected = `${line1}${line2}${line3}`;
+      const actual = pipe.transform(query);
+      expect(actual).toBe(expected);
+    });
+
+    it('returns empty string for invalid data', () => {
+      const query = {
+        unexpectedField: 'should not be here',
+        unexpectedArray: [{ badField: 'not valid' }],
+      };
+      const actual = pipe.transform(query);
+      expect(actual).toBe('');
+    });
   });
 });
