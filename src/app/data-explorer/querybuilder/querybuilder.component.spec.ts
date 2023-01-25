7f435d0500ee041a860e058bb851f4dcbe197f3b
Peter Monks <Peter.Monks@oxfordcc.co.uk>
2023-01-25 12:00:23 +0000

    gh-172 Tests added to QueryBuilderComponent

--- src/app/data-explorer/querybuilder/querybuilder.component.spec.ts
+++ src/app/data-explorer/querybuilder/querybuilder.component.spec.ts
@@ -17,36 +17,221 @@ limitations under the License.
 SPDX-License-Identifier: Apache-2.0
 */
 import { QueryBuilderComponent } from './querybuilder.component';
-import { MdmResourcesConfiguration } from '@maurodatamapper/mdm-resources';
-import { MeqlPipe } from '../pipes/meql.pipe';
+import {
+  QueryBuilderComponent as LibQueryBuilderComponent,
+  Rule,
+} from 'angular2-query-builder';
 import {
   ComponentHarness,
   setupTestModuleForComponent,
 } from 'src/app/testing/testing.helpers';
+import { createTerminologyServiceStub } from 'src/app/testing/stubs/terminology.stub';
+import { TerminologyService } from 'src/app/mauro/terminology.service';
+import { MockComponent } from 'ng-mocks';
+import { MeqlOutputComponent } from '../meql-output/meql-output.component';
+import { MatCard } from '@angular/material/card';
+import { QueryCondition } from '../data-explorer.types';
+import { mapModelDataTypeToOptionsArray } from '../query-builder.service';
+import { CatalogueItemDomainType } from '@maurodatamapper/mdm-resources';
+import { of } from 'rxjs';
+import { AutocompleteSelectOptionSet } from 'src/app/shared/autocomplete-select/autocomplete-select.component';
 
 describe('QueryBuilderComponent', () => {
   let harness: ComponentHarness<QueryBuilderComponent>;
-  const mdmResourcesConfiguration = new MdmResourcesConfiguration();
+  const terminologyStub = createTerminologyServiceStub();
 
-  const setupComponentTest = async () => {
-    return await setupTestModuleForComponent(QueryBuilderComponent, {
+  beforeEach(async () => {
+    harness = await setupTestModuleForComponent(QueryBuilderComponent, {
       providers: [
         {
-          provide: MdmResourcesConfiguration,
-          useValue: mdmResourcesConfiguration,
+          provide: TerminologyService,
+          useValue: terminologyStub,
         },
       ],
-      declarations: [MeqlPipe],
+      declarations: [
+        MockComponent(MeqlOutputComponent),
+        MockComponent(MatCard),
+        MockComponent(LibQueryBuilderComponent),
+      ],
     });
-  };
+  });
 
-  describe('creation', () => {
-    beforeEach(async () => {
-      harness = await setupComponentTest();
+  it('should create', () => {
+    expect(harness.isComponentCreated).toBeTruthy();
+    expect(harness.component.dataElements).toStrictEqual([]);
+    expect(harness.component.color).toBe('primary');
+    expect(harness.component.query).toStrictEqual({
+      condition: 'and',
+      rules: [],
+    });
+    expect(harness.component.config).toStrictEqual({
+      fields: {},
+    });
+  });
+
+  describe('has fields', () => {
+    it('should be false when fields list is empty', () => {
+      expect(harness.component.hasFields).toBeFalsy();
     });
 
-    it('should be created', () => {
-      expect(harness.component).toBeTruthy();
+    it('should be true when fields list is not empty', () => {
+      harness.component.config = {
+        fields: {
+          testField: {
+            name: 'testField',
+            type: 'string',
+          },
+        },
+      };
+
+      expect(harness.component.hasFields).toBeTruthy();
+    });
+  });
+
+  describe('initialisation', () => {
+    it('should not initialise any term search results if there are no terminology fields', () => {
+      harness.component.config = {
+        fields: {
+          testField: {
+            name: 'testField',
+            type: 'string',
+          },
+        },
+      };
+
+      harness.component.ngOnInit();
+
+      expect(Object.keys(harness.component.termSearchResults)).toStrictEqual([]);
+    });
+
+    it('should initialise term search results for a terminology field', () => {
+      harness.component.config = {
+        fields: {
+          testField1: {
+            name: 'testField1',
+            type: 'terminology',
+          },
+          testField2: {
+            name: 'testField2',
+            type: 'terminology',
+          },
+        },
+      };
+
+      harness.component.ngOnInit();
+
+      const expectedSetup = {
+        count: 0,
+        options: [],
+      };
+
+      expect(harness.component.termSearchResults['testField1']).toStrictEqual(
+        expectedSetup
+      );
+      expect(harness.component.termSearchResults['testField2']).toStrictEqual(
+        expectedSetup
+      );
+    });
+
+    it('should reset the query when empty', () => {
+      harness.component.query = {
+        condition: 'or',
+        rules: [],
+      };
+
+      harness.component.ngOnInit();
+
+      expect(harness.component.query).toStrictEqual({
+        condition: 'and',
+        rules: [],
+      });
+    });
+
+    it('should not reset the query when it has a value', () => {
+      const query: QueryCondition = {
+        condition: 'or',
+        rules: [
+          {
+            field: 'test',
+            operator: '=',
+            value: 'value',
+          },
+          {
+            field: 'something',
+            operator: '=',
+            value: 'else',
+          },
+        ],
+      };
+
+      harness.component.query = query;
+      harness.component.ngOnInit();
+      expect(harness.component.query).toStrictEqual(query);
+    });
+
+    describe('model changed', () => {
+      it('should raise an event when the query changes', () => {
+        const spy = jest.spyOn(harness.component.queryChange, 'emit');
+
+        const query: QueryCondition = {
+          condition: 'or',
+          rules: [
+            {
+              field: 'test',
+              operator: '=',
+              value: 'value',
+            },
+          ],
+        };
+
+        harness.component.modelChanged(query);
+        expect(spy).toHaveBeenCalledWith(query);
+      });
+    });
+
+    describe('term search changed', () => {
+      const options = mapModelDataTypeToOptionsArray({
+        id: '123',
+        domainType: CatalogueItemDomainType.ModelDataType,
+        label: 'data type',
+        modelResourceDomainType: CatalogueItemDomainType.Terminology,
+        modelResourceId: '456',
+      });
+
+      const rule: Rule = {
+        field: 'test-field',
+      };
+
+      it('should set returned term search results', () => {
+        const expectedResults: AutocompleteSelectOptionSet = {
+          count: 10,
+          options: [
+            {
+              name: 'definition',
+              value: {
+                domainType: CatalogueItemDomainType.Term,
+                definition: 'definition',
+                code: 'code',
+              },
+            },
+          ],
+        };
+
+        expect(harness.component.termSearchResults[rule.field]).toBeUndefined();
+
+        terminologyStub.listTerms.mockImplementationOnce(() => {
+          return of({
+            count: expectedResults.count,
+            items: expectedResults.options.map((opt) => opt.value),
+          });
+        });
+
+        harness.component.termSearchChanged('search value', rule, options);
+
+        expect(harness.component.termSearchResults[rule.field]).toStrictEqual(
+          expectedResults
+        );
+      });
     });
   });
 });
